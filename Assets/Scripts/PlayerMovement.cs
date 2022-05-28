a82e8916d9c312e8cbe4c032ecbe75c81dadb04f
Martin <likuidaudio@gmail.com>
2022-05-28 17:40:24 -0300

    Lightersound

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     //fmod yo el mas capo
 
     [SerializeField] EventReference stepsound;
+    [SerializeField] EventReference lightersound;
     public FMOD.Studio.EventInstance playerState;
 
 
@@ -94,6 +95,7 @@ public class PlayerMovement : MonoBehaviour
         {
             lighter.SetActive(true);
             lighterIsOn = true;
+            FMODUnity.RuntimeManager.PlayOneShot(lightersound);
         }
             
     }
